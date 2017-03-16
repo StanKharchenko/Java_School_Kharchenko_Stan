16a1aada846c5160cc3a00c945db5f655e82bf72
KharchenkoStan <Stan.Kharchenko@gmail.com>
2017-03-16 18:30:19 +0300

    ыываоывдалоыдвла
    Signed-off-by: KharchenkoStan <Stan.Kharchenko@gmail.com>

--- lesson1javaintroduction/src/main/java/JavaSchool/A.java
+++ lesson1javaintroduction/src/main/java/JavaSchool/A.java
@@ -1,8 +1,13 @@
+package JavaSchool;
+
+import JavaSchool.Utils.B;
+
 /**
  * Created by 12 on 16.03.2017.
  */
 public class A {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        new B().out();
     }
 }
---
+++ lesson1javaintroduction/src/main/java/JavaSchool/Utils/B.java
@@ -0,0 +1,11 @@
+package JavaSchool.Utils;
+
+/**
+ * Created by 12 on 16.03.2017.
+ */
+public class B {
+    public static void out() {
+        System.out.println("IN B");
+    }
+
+}
