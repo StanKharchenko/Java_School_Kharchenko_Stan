1b87dbe5dae0df3820067997971fb740978c5ba5
StanKharchenko <stan.kharchenko@gmail.com>
2017-03-30 15:06:13 +0300

    Create Hw03part2.java

---
+++ lesson4/src/main/java/Hw03part2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Hw03part2 {
+    static final String FILE_PATH = "C:\\Temp\\1.txt";
+    static boolean isFirst = true;
+
+    public static void main(String[] args) throws IOException {
+        NumsOperator nOp = new NumsOperator();
+        Files.lines(Paths.get(FILE_PATH)).forEach(s -> parse(s, nOp));
+    }
+    private static void parse(String row, NumsOperator nOp){
+        if(isFirst == true){
+            isFirst = !isFirst;
+            return;
+        }
+        String[] chars = row.split(" ");
+        if (Integer.parseInt(chars[0]) == 1){
+            nOp.addItem(Integer.parseInt(chars[1]));
+        }else
+        {
+            System.out.println(nOp.removeMin());
+        }
+    }
+}
