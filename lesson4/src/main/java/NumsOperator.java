f9c907b5fab979e530c76289ede5e84d50078dc8
StanKharchenko <stan.kharchenko@gmail.com>
2017-03-30 15:06:47 +0300

    Create NumsOperator.java

---
+++ lesson4/src/main/java/NumsOperator.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class NumsOperator {
+    private ArrayList<Integer> list = new ArrayList<>();
+
+    public void addItem(Integer num){
+        list.add(num);
+        Collections.sort(list);
+    }
+    public Integer removeMin(){
+        Integer result = list.get(0);
+        list.remove(0);
+        return result;
+    }
+}
